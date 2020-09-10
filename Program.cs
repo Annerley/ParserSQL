a3e447506c9fe4f4386d2980454f69d606c88a86
Дарья Лапутина <laputina975@gmail.com>
2020-09-10 16:11:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ParserSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tmp;
+            string path = "../../../sql.csv";
+
+            Dictionary<int, Dictionary<string,string>> strings = new Dictionary<int, Dictionary<string, string>>();
+            Dictionary<string, string> values = new Dictionary<string, string>();
+
+
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string row;
+                
+                string columns_str = reader.ReadLine();
+                string[] columns = columns_str.Split(";");
+                int i = 0;
+                while ((row = reader.ReadLine()) != null)
+                {
+                    tmp = row.Split(';');
+                    for (int j = 0; j < tmp.Length; j++)
+                    {
+                        values[columns[j]] =  tmp[j];
+                        
+                        Console.WriteLine(columns[j] + " " + tmp[j]);
+                    }
+                    strings[i] = values;
+
+                    i++;
+                    Console.WriteLine("!");
+                   
+                    
+                }
+            }
+
+         
+            
+           
+            
+            
+        }
+    }
+}
